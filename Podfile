f73f248ab6baed13a65572ebd71f9f40eaf4455b
lichao <lichao3@ysbang.cn>
2021-08-28 16:30:03 +0800

    coacoapods 管理项目

---
+++ Podfile
@@ -0,0 +1,14 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios,'10.0'
+
+target 'TestGitReposity' do
+   use_frameworks!
+inhibit_all_warnings!
+
+pod 'ObjectMapper','~>3.5.1'
+pod 'HandyJSON','~>5.0.0’
+pod 'Moya','~>13.0.1’
+pod 'Kingfisher','~>5.7.1’
+pod 'SnapKit','~>5.0.1’
+
+end
--- TestGitReposity/ViewController.swift
+++ TestGitReposity/ViewController.swift
@@ -8,15 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red;
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated);
         
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews();
+    }
+    
+    func justTset() -> Void {
+        print("justTset");
     }
 
 }
