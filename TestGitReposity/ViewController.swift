3949923465cfba7d093116a81de0be75874a5d23
lichao <lichao3@ysbang.cn>
2021-08-28 11:35:29 +0800

    创建git项目

---
+++ TestGitReposity/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TestGitReposity
+//
+//  Created by lichao on 2021/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+    }
+
+}
+
